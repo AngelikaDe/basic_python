ce18634a2219f0554f493650fc0a24f7c743b677
Анжелика Демина <angelika@MacBook-Pro-Anzelika-3.local>
2023-11-06 17:24:00 +0300

    00 ex

--- day00/ex00.py
+++ day00/ex00.py
@@ -1,11 +1,15 @@
 import sys
 
-if __name__ == '__main__':
-    num_lines = 0
+if __name__ == '__main':
+    num_lines = None
     count = 0
     if len(sys.argv) > 1:
         num_lines = int(sys.argv[1])
 
     for line in sys.stdin:
-        if len(line) == 33 and line[5] != '0' and ('00000' in line[0:5]):
-            print(line)
+        if num_lines is None or count < num_lines:
+            if len(line) == 33 and line[5] != '0' and '00000' in line[0:5]:
+                print(line)
+                count += 1
+        else:
+            break
