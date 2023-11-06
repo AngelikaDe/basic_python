c1e0055d961c9a4f9d4b10eba41d2495e3eb365c
Анжелика Демина <angelika@MacBook-Pro-Anzelika-3.local>
2023-11-06 20:07:32 +0300

    tests for ex00

--- day01/tests.py
+++ day01/tests.py
@@ -1,21 +1,20 @@
 import unittest
-import ex01, ex00
+from ex00 import add_ingot, get_ingot, empty
+
 
 class TestEx00(unittest.TestCase):
     def test_add_get_empty(self):
         purse = {}
         res = add_ingot(get_ingot(add_ingot(empty(purse))))
-        self.assertEqal(res.get("gold_ingots"),1)
+        self.assertEqual(res.get("gold_ingots"), 1)
 
     def test_add_get(self):
         purse = {'gold_ingots': 10}
         res = add_ingot(get_ingot(add_ingot(purse)))
-        self.assertEqal(res.get("gold_ingots"),11)
-
+        self.assertEqual(res.get("gold_ingots"), 11)
 
 class TestEx01(unittest.TestCase):
     pass
-
 
 if __name__ == '__main__':
     unittest.main()
