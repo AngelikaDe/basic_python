bd0a47256cf9f586c5d31f2c53ba32eec5727988
Alcestis Another <alcestis@il-c2.msk.21-school.ru>
2023-11-06 17:15:01 +0300

    end line

--- day00/ex00.py
+++ day00/ex00.py
@@ -1,14 +1,11 @@
-# 0000071f49cffeaea4184be3d507086v
+import sys
 
-# import sys
-# num_lines = 0
-# if len(sys.orig_argv) > 2:
-#     num_lines = sys.orig_argv[2]
-import fileinput
+if __name__ == '__main__':
+    num_lines = 0
+    count = 0
+    if len(sys.argv) > 1:
+        num_lines = int(sys.argv[1])
 
-for line in fileinput.input():
-    print(line[0:5])
-    print(line[5])
-    print(len(line))
-    if len(line) == 33 and line[5] != '0' and line[0:4] == '00000':
-        print("HEHEH")
+    for line in sys.stdin:
+        if len(line) == 33 and line[5] != '0' and ('00000' in line[0:5]):
+            print(line)
